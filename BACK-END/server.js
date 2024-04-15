269c999a5ec8e9ad6eee7b48851e3ee8ec9b8d15
SUYOG-S2 <suyoggouda757@gmail.com>
2024-04-15 11:30:12 +0530

    Updated Schema

--- BACK-END/server.js
+++ BACK-END/server.js
@@ -1,13 +1,31 @@
 import express from "express";
 import Connection from "./Connection.js";
+import User from "./Schema/User.js";
+import bodyParser from "body-parser";
 
 let app = express();
-
-
+app.use(bodyParser.json())
 const port = 8000;
-app.get('/',(req,res) => {
-    res.send("Fine as Hell......");
+
+app.get('/',async (req,res) => {
+    // res.send("Fine as Hell......");
+
+    let data = await User.find();
+    res.send(data);
 });
+
+app.post("/post",async (req,res) => {
+
+  let body = req.body;
+  console.log(body)
+
+  try{
+    let value = await User.insertMany(body);
+    res.status(201).send({message : "data inserted",value})
+  }catch(error){
+    console.log(error)
+  }
+})
 
 Connection().then(() => {
 app.listen(port, () => {
