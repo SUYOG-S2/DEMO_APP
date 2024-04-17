66e8a75cc945effe7142b57da49649c16cef44a2
SUYOG-S2 <suyoggouda757@gmail.com>
2024-04-17 16:26:14 +0530

    Updated All Requests

--- BACK-END/server.js
+++ BACK-END/server.js
@@ -7,22 +7,55 @@ let app = express();
 app.use(bodyParser.json())
 const port = 8000;
 
-app.get('/',async (req,res) => {
+app.get('/', async (req,res) => {
     // res.send("Fine as Hell......");
 
     let data = await User.find();
     res.send(data);
 });
 
-app.post("/post",async (req,res) => {
+app.post("/post", async (req,res) => {
 
   let body = req.body;
-  console.log(body)
+  // console.log(body);
+
+  let data = await User.find({ email : body.email });
+  // console.log(data,"Same Thing");
+  if (data.length > 0) {
+    return res.status(401).send({ message : "Oops! The user already exists" });
+  }
+    
 
   try{
     let value = await User.insertMany(body);
-    res.status(201).send({message : "data inserted",value})
-  }catch(error){
+    res.status(201).send({ message : "Data Inserted",value })
+
+  }catch(error) {
+    console.log(error)
+  }
+})
+
+app.patch("/update/:id", async (req,res) => { 
+    let body = req.body;
+    let id = req.params.id;
+    // console.log(id);
+    try{
+      let value = await User.findByIdAndUpdate(id, body, { new : true });
+      res.status(201).send({ message : "Data Updated", value })
+
+    }catch(error) {
+      console.log(error)
+    }
+})
+
+app.delete("/delete/:id", async (req,res) => {
+
+  try{
+    let id = req.params.id;
+    let data = await User.findByIdAndDelete(id);
+    res.status(201).send({ message : "Data Deleted", data })
+
+  }catch(error) {
     console.log(error)
   }
 })
